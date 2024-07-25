75b848012ffa5e526d298a4c923e07e43a20ff8a
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2024-07-25 21:23:02 +0200

    fix: stream import system

--- database/seeders/message_seeder.ts
+++ database/seeders/message_seeder.ts
@@ -1,109 +1,43 @@
-import Message, { type MessageData } from '#models/message'
-import Stream from '#models/stream'
-import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-type JsonData = MessageData[]
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
-const streamData = [
-  {
-    date: new Date('2024-06-23'),
-    twitchId: '2179711073',
-  },
-  {
-    date: new Date('2024-06-24'),
-    twitchId: '2180393607',
-  },
-  {
-    date: new Date('2024-06-26'),
-    twitchId: '2182258117',
-  },
-  {
-    date: new Date('2024-07-04'),
-    twitchId: '2189218512',
-  },
-  {
-    date: new Date('2024-07-07'),
-    twitchId: '2191657978',
-  },
-  {
-    date: new Date('2024-07-08'),
-    twitchId: '2192465393',
-  },
-  {
-    date: new Date('2024-07-09'),
-    twitchId: '2193494493',
-  },
-  {
-    date: new Date('2024-07-10'),
-    twitchId: '2194149182',
-  },
-  {
-    date: new Date('2024-07-14'),
-    twitchId: '2194976969',
-  },
-  {
-    date: new Date('2024-07-15'),
-    twitchId: '2197523249',
-  },
-  {
-    date: new Date('2024-07-16'),
-    twitchId: '2198422981',
-  },
-  {
-    date: new Date('2024-07-18'),
-    twitchId: '2199302922',
-  },
-  {
-    date: new Date('2024-07-19'),
-    twitchId: '2201090026',
-  },
-  {
-    date: new Date('2024-07-20'),
-    twitchId: '2202072512',
-  },
-  {
-    date: new Date('2024-07-23'),
-    twitchId: '2205383277',
-  },
-]
+import Stream from '#models/stream'
+import Message, { type MessageData } from '#models/message'
 
 export default class extends BaseSeeder {
   async run() {
     const dataFolder = path.join(path.dirname(import.meta.url), '/data')
     const dataFolderUrl = fileURLToPath(dataFolder)
 
-    const streams = await Stream.createMany(streamData)
+    const filenames = fs.readdirSync(dataFolderUrl)
 
-    const messagesData = [] as Partial<Message>[]
-
-    fs.readdir(dataFolderUrl, async (err, files) => {
-      if (err) {
-        return console.error(err)
-      }
-      for (const file of files) {
-        const filePath = path.join(dataFolderUrl, file)
-        const data = fs.readFileSync(filePath, 'utf-8')
-        const parsedData = JSON.parse(data) as JsonData
-        const streamDate = new Date(file.split('.')[0])
-
-        const stream = streams.find((el) => el.date.getTime() === streamDate.getTime())
-        const matchRegexp = new RegExp('(?:^@.* \\|\\| .*)|(?:^\\|\\| .*)', 'g')
-        parsedData.map((jsonData) => {
-          if (!matchRegexp.test(jsonData.message)) return
-
-          messagesData.push({
+    for (const file of filenames) {
+      const filePath = path.join(dataFolderUrl, file)
+      const data = fs.readFileSync(filePath, 'utf-8')
+      const parsedData: MessageData[] = JSON.parse(data)
+      const streamID = file.split('.')[0]
+      const date = new Date(parsedData[0].timestamp / 1000)
+      console.log(
+        `Processing stream https://www.twitch.tv/videos/${streamID} from ${date.toLocaleDateString()}`
+      )
+      const stream = await Stream.firstOrCreate(
+        { date, twitchId: streamID },
+        { date, twitchId: streamID }
+      )
+      const matchRegexp = new RegExp('(?:^@.* \\|\\| .*)|(?:^\\|\\| .*)', 'g')
+      for (const jsonData of parsedData) {
+        if (matchRegexp.test(jsonData.message)) {
+          await Message.create({
             timestamp: jsonData.timestamp,
             data: jsonData,
             twitchId: jsonData.message_id,
-            streamId: stream?.id,
+            streamId: stream.id,
           })
-        })
+        }
       }
-    })
-
-    await Message.createMany(messagesData)
+    }
   }
 }
---
+++ scripts/import_all_streams.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env bash
+
+STREAM_IDS=(
+  '2179711073'
+  '2180393607'
+  '2182258117|04:45:00'
+  '2189218512|03:18:00'
+  '2191657978'
+  '2192465393'
+  '2193494493'
+  '2194149182'
+  '2194976969'
+  '2197523249'
+  '2198422981'
+  '2199302922'
+  '2201090026'
+  '2202072512'
+  '2205383277'
+)
+
+for STRING in ${STREAM_IDS[@]}; do
+  IFS='|' read -r ID TIME <<<"$STRING"
+
+  OUTPUT="$(dirname "$0")/../database/seeders/data/$ID.json"
+  if [ -f $OUTPUT ]; then
+    echo "File $OUTPUT already exists. Skipping..."
+    continue
+  fi
+
+  VIDEO_URL="https://www.twitch.tv/videos/$ID"
+  if [ -z "$TIME" ]; then
+    TITLE="Downloading chat of $VIDEO_URL"
+    SUCCESS_MESSAGE="Successfully downloaded chat of $VIDEO_URL"
+    CMD_ARGS="$VIDEO_URL --output $OUTPUT --message_types text_message --quiet"
+  else
+    TITLE="Downloading chat of $VIDEO_URL from $TIME"
+    SUCCESS_MESSAGE="Successfully downloaded chat of $VIDEO_URL from $TIME"
+    CMD_ARGS="$VIDEO_URL --output $OUTPUT --message_types text_message --quiet --start_time $TIME"
+  fi
+
+  gum spin --spinner dot --title "$TITLE" -- chat_downloader $CMD_ARGS
+
+  echo $SUCCESS_MESSAGE
+done
+
+echo "All streams imported successfully!"
+exit 0
